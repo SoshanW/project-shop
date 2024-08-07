e1f6d57107744e82151ee33b8d046499005ee21b
Soshan Wijayarathne <soshanw123@gmail.com>
2024-08-07 21:32:26 +0530

    Updated and completed Login And Signup

--- src/com/dev/pos/controller/LoginFormController.java
+++ src/com/dev/pos/controller/LoginFormController.java
@@ -5,7 +5,6 @@ import com.dev.pos.db.DBConnection;
 import com.dev.pos.dto.UserDTO;
 import com.dev.pos.util.security.PasswordManager;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -13,7 +12,6 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -22,44 +20,37 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public PasswordField txtPassword;
 
-    @FXML
-    private AnchorPane context;
+    public void btnLoginOnAction(ActionEvent actionEvent) {
 
-    @FXML
-    private TextField txtEmail;
-
-    @FXML
-    private PasswordField txtPassword;
-
-    @FXML
-    void btnLoginOnAction(ActionEvent event) throws SQLException, ClassNotFoundException {
-        try{
+        try {
             UserDTO user = DatabaseAccessCode.findUser(txtEmail.getText());
 
-            if (user!=null){
-            if(PasswordManager.checkPassword(txtPassword.getText(), user.getPassword())){
-                setUi("DashboardForm");
-            }else {
-                new Alert(Alert.AlertType.INFORMATION, "User Not Found...!").show();
-            }
+            if(user!=null){
+                if(PasswordManager.checkPassword(txtPassword.getText(),user.getPassword())){
+                    setUI("DashboardForm");
+                }else {
+                    new Alert(Alert.AlertType.INFORMATION,"User Not Found...!").show();
+                }
             }
 
-        }catch(ClassNotFoundException | SQLException | IOException e){
+        }catch (ClassNotFoundException | SQLException | IOException e){
             e.printStackTrace();
         }
 
     }
 
-    @FXML
-    void btnSignUpOnAction(ActionEvent event) throws IOException {
-        setUi("SignUp");
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+
+        setUI("SignupForm");
     }
 
-    private void setUi(String location) throws IOException {
+    private void setUI(String location) throws IOException {
         Stage stage = (Stage) context.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"))));
         stage.centerOnScreen();
     }
-
 }
