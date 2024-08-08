efc120efbc756897a93c7bb6abecac6e1c3f05bc
Soshan Wijayarathne <soshanw123@gmail.com>
2024-08-08 08:10:40 +0530

    Completed ProductMainForm UI

--- src/com/dev/pos/controller/ProductMainFormController.java
+++ src/com/dev/pos/controller/ProductMainFormController.java
@@ -1,8 +1,13 @@
 package com.dev.pos.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ProductMainFormController {
     public AnchorPane context;
@@ -36,5 +41,12 @@ public class ProductMainFormController {
     }
 
     public void btnNewBatchOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUI(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
     }
 }
