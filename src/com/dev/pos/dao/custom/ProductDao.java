d2f2e0d32747c1e26d7427d67f2e3f9ca2525f8b
Soshan Wijayarathne <soshanw123@gmail.com>
2024-09-02 10:57:46 +0530

    Created product crud in ProductDao

--- src/com/dev/pos/dao/custom/ProductDao.java
+++ src/com/dev/pos/dao/custom/ProductDao.java
@@ -1,4 +1,22 @@
 package com.dev.pos.dao.custom;
 
+import com.dev.pos.Enitity.Product;
+
+import java.util.List;
+
 public interface ProductDao {
+
+    public boolean saveProduct(Product product);
+
+    public boolean updateProduct(Product product);
+
+    public boolean deleteProduct(int code);
+
+    public Product findProduct(int code);
+
+    public List<Product> findAllProducts();
+
+    public List<Product> searchProduct(int code);
+
+    public int getLastProductId();
 }
