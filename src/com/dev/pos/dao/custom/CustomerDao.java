779822823dd5027e8c412cd380e1e2c922fcdaec
Soshan Wijayarathne <soshanw123@gmail.com>
2024-09-02 10:53:53 +0530

    Created customer crud in CustomerDao

--- src/com/dev/pos/dao/custom/CustomerDao.java
+++ src/com/dev/pos/dao/custom/CustomerDao.java
@@ -1,4 +1,20 @@
 package com.dev.pos.dao.custom;
 
+import com.dev.pos.Enitity.Customer;
+
+import java.util.List;
+
 public interface CustomerDao {
+
+    public boolean saveCustomer(Customer customer);
+
+    public boolean updateCustomer(Customer customer);
+
+    public boolean deleteCustomer(String email);
+
+    public Customer findCustomer(String email);
+
+    public List<Customer> findAllCustomer();
+
+    public List<Customer> searchCustomer();
 }
