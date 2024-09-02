b87173646827de581f1e278821f9fe45a9b6f5f6
Soshan Wijayarathne <soshanw123@gmail.com>
2024-09-02 11:04:51 +0530

    Created and Implemented Impl files CustomerDaoImpl, ProductDaoImpl and UserDaoImpl

---
+++ src/com/dev/pos/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,43 @@
+package com.dev.pos.dao.impl;
+
+import com.dev.pos.Enitity.Product;
+import com.dev.pos.dao.custom.ProductDao;
+
+import java.util.List;
+
+public class ProductDaoImpl implements ProductDao {
+    @Override
+    public boolean saveProduct(Product product) {
+        return false;
+    }
+
+    @Override
+    public boolean updateProduct(Product product) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteProduct(int code) {
+        return false;
+    }
+
+    @Override
+    public Product findProduct(int code) {
+        return null;
+    }
+
+    @Override
+    public List<Product> findAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> searchProduct(String value) {
+        return List.of();
+    }
+
+    @Override
+    public int getLastProductId() {
+        return 0;
+    }
+}
